81dfa316d8a3800b2c7bbab800ce500ced26044a
Mirashi <shraddha.s.mirashi@gmail.com>
2018-06-08 12:55:51 +0530

    CORS Handling

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const app = express();
 const expensesRoutes = require('./api/routes/expenses');
 const categoriesRoutes = require('./api/routes/categories');
 const subcategoriesRoutes = require('./api/routes/subcategories');
+app.use(parser.urlencoded({extended:false}));
 app.use(parser.json());
 app.use(morgan());
+
+//CORS
+app.use((req,res,next)=>{
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	if(req.mothod==='OPTIONS'){
+		res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,FETCH');
+		return res.status(200).json({});
+	}
+	next();
+});
+
 app.use('/expenses',expensesRoutes);
 app.use('/categories',categoriesRoutes);
 app.use('/subcategories',subcategoriesRoutes);
