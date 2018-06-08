c0423a4868d735dd0ed345ffbbb7ed65052faaf9
Mirashi <shraddha.s.mirashi@gmail.com>
2018-06-08 11:37:16 +0530

    Error Handling

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const parser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 
 const expensesRoutes = require('./api/routes/expenses');
 const categoriesRoutes = require('./api/routes/categories');
 const subcategoriesRoutes = require('./api/routes/subcategories');
 app.use(parser.json());
-//app.use('/expenses',expensesRoutes);
-app.use(parser.json());
+app.use(morgan());
+app.use('/expenses',expensesRoutes);
 app.use('/categories',categoriesRoutes);
 app.use('/subcategories',subcategoriesRoutes);
-/*app.use((req,res,next)=>{
-    res.status(200).json(
-        {
-            messaage : 'It works'
-        }
-    );
-});*/
+
+app.use((req,res,next)=>{
+	const error = new Error('Not found');
+	error.status(404);
+	next(error);
+});
+
+app.use((error,req,res,next)=>{
+	res.status(error.status || 500);
+	res.json({
+		error : {
+			messsage : error.message
+		}
+	})
+});
 
 module.exports = app;
